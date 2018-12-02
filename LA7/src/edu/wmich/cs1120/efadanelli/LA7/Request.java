2462a2517e36971de7be4789911930e6130cbf25
Evan Fadanelli <Evan Fadanelli@192.168.2.5>
2018-12-02 15:38:09 -0500

    Evan worked on Request

--- LA7/src/edu/wmich/cs1120/efadanelli/LA7/Request.java
+++ LA7/src/edu/wmich/cs1120/efadanelli/LA7/Request.java
@@ -12,12 +12,29 @@ public class Request<T> implements Comparable<T>{
 	// Constructor
 	public Request(String studentName, String studentDept, String studentLevel,
 				String courseDept, int courseNumber, double[][] GPA_Array) {
-		
+		name = studentName;
+		dept = studentDept;
+		level = studentLevel;
+		this.courseDept = courseDept;
+		this.courseNumber = courseNumber;
+		this.GPA_Array = GPA_Array;
 	}
 	// Returns number of years to graduation (0 for seniors, 1 for juniors etc.). This is determined from the 
 	// student’s level – senior, junior, etc.
 	public int yearsFromGraduation(String level) {
-		return 0;
+		if(level.equals("Senior"))
+			return 0;
+		else {
+			if(level.equals("Junior"))
+				return 1;
+			else {
+				if(level.equals("Sophmore"))
+					return 2;
+				else {
+						return 3;
+				}
+			}
+		}
 	}
 
 	// Calculate the GPA for a particular student.
